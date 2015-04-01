c31209e7063d0213debf7917a05f108275e2337c
mollihua <mollihua@users.noreply.github.com>
2015-04-01 11:23:31 -0700

    change syntax

--- dist_nearest_neighbor.py
+++ dist_nearest_neighbor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Purpose: Plot time series and histograms of all pairs of dppc P atoms that are initially nearest neighbors
-# Syntax: ./dist_dppc.py test.xyz
+# Syntax: ./dist_nearest_neighbor.py test.xyz
 # Note: no periodic boundary condition is considered yet
 
 
